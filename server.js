9cb6c9e57282ad7c1baee397522303711d8821e5
Santosh Yadav <santosh.yadav198613@gmail.com>
2017-08-29 18:51:33 +0530

    added CRUD operation

--- server.js
+++ server.js
@@ -6,29 +6,37 @@ const productAPI = require('./user/productAPI');
 const employeeAPI = require('./user/employeeApi');
 const myLogger = require('./middleware/myLogger');
 var fs = require('fs');
+var mongoClient = require('mongodb').MongoClient;
+ObjectID = require('mongodb').ObjectID;
 
-app.use(function(req, res, next) {
+app.use(myLogger);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoClient.connect('mongodb://localhost:27017/API', function (err, db) {
+  userAPI(app);
+  productAPI(app);
+  employeeAPI(app, db, ObjectID);
+});
+
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.use(myLogger);
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
 
 
 app.get('/', function (req, res) {
- 
-  var sample = fs.readFileSync('sample.txt','utf8');
-  fs.writeFileSync('output.txt',sample);
+
+  var sample = fs.readFileSync('sample.txt', 'utf8');
+  fs.writeFileSync('output.txt', sample);
   res.send(sample);
 });
 
-userAPI(app);
-productAPI(app);
-employeeAPI(app);
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
--- user/employeeApi.js
+++ user/employeeApi.js
@@ -1,19 +1,41 @@
-module.exports = function (app) {
+module.exports = function (app, db, ObjectID) {
 
-  app.get('/api/employee', function (req, res) {
+    app.get('/api/employee', function (req, res) {
         res.send('this is employee');
     });
 
-    app.get('/api/employee/:id', function (req, res) {
-        res.send(req.params);
+    app.get('/api/employee/:_id', function (req, res) {
+        console.log(req.params);
+        console.log(ObjectID);
+        var ObjectId = new ObjectID(req.params._id);
+        console.log(ObjectId);
+        db.collection('employee').find({ _id: ObjectId }).toArray(function (err, result) {
+            console.log(result);
+            res.send(result);
+        });
     });
 
     app.post('/api/employee', function (req, res) {
-      
-        res.send(req.body);
+        console.log(req.body);
+        db.collection('employee').insertOne(req.body).then(function (result) {
+            console.log(result);
+            res.send(result.insertedId);
+        })
     });
 
-     app.delete('/api/employee', function (req, res) {
+    app.put('/api/employee', function (req, res) {
+            console.log(req.body);
+        db.collection('employee').updateOne(
+            { name: 'Santosh' },
+            {
+                $set: { adress: 'pune' }
+            }).then(function (result) {
+                console.log(result);
+                res.send(result);
+            })
+    });
+
+    app.delete('/api/employee', function (req, res) {
         res.send(req.body);
     });
 
